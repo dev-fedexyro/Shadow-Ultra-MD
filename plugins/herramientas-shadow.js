54cc868ff944feb9e135b11c65a5b4c6304619f4
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-22 17:05:01 -0300

    Update herramientas-shadow.js

--- plugins/herramientas-shadow.js
+++ plugins/herramientas-shadow.js
@@ -1,31 +1,32 @@
-import { fileURLToPath } from 'url'
-import { watch } from 'fs'
+import { fileURLToPath} from 'url'
+import { watch} from 'fs'
 
-const videoUrl = 'https://cdn.russellxz.click/14cf14e9.mp4';
+const videoUrls = [
+    'https://cdn.russellxz.click/14cf14e9.mp4',
+    'https://cdn.russellxz.click/17b9d337.mp4',
+    'https://cdn.russellxz.click/7e2cb594.mp4'
+];
 
-const handler = async (m, { conn, command }) => {
-    
+const handler = async (m, { conn, command}) => {
     if (command === 'shadow') {
-        
-        const caption = '👻 Vídeo de Shadow'; 
+        const caption = '👻 Vídeo de Shadow';
+        const randomUrl = videoUrls[Math.floor(Math.random() * videoUrls.length)];
 
         try {
-
-          await conn.sendMessage(m.chat, {
-                video: { url: videoUrl },
+            await conn.sendMessage(m.chat, {
+                video: { url: randomUrl},
                 caption: caption,
                 mimetype: 'video/mp4'
-            }, { quoted: m });
-
-        } catch (error) {
+}, { quoted: m});
+} catch (error) {
             console.error('Error al enviar el video de Shadow:', error);
             m.reply('❌ Lo siento, hubo un error al enviar el video.');
-        }
-    }
+}
+}
 };
 
-handler.help = ['shadow']; 
-handler.tags = ['herramientas']; 
-handler.command = ['shadow']; 
+handler.help = ['shadow'];
+handler.tags = ['herramientas'];
+handler.command = ['shadow'];
 
 export default handler;
