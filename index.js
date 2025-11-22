817ed8c4d8b2d2d6eff3acca7755cfd185548e19
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-22 19:54:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ return createRequire(dir)
 global.timestamp = { start: new Date() }
 const __dirname = global.__dirname(import.meta.url)
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-global.prefix = new RegExp('^[/]')
+global.prefix = new RegExp('^[/.]')
 
 global.db = new Low(/https?:\/\//.test(global.opts['db'] || '') ? new cloudDBAdapter(global.opts['db']) : new JSONFile('database.json'))
 global.DATABASE = global.db
