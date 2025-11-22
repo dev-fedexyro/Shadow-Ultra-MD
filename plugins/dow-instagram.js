fb2d89cf5cdf0c4c0b71dcfec1977b1730cf83fe
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-22 18:25:52 -0300

    Update dow-instagram.js

--- plugins/dow-instagram.js
+++ plugins/dow-instagram.js
@@ -1,1 +1,74 @@
+const handler = async (m, { args, conn, usedPrefix, command}) => {
+  try {
+    if (!args[0]) {
+      return conn.reply(m.chat, `\`\`\`🌱 Por favor, ingresa un enlace de *Instagram* o *Facebook*.\`\`\``, m)
 
+    let data = []
+    const url = encodeURIComponent(args[0])
+    await m.react('⏳')
+
+    if (/(instagram\.com)/i.test(args[0])) {
+      try {
+        const api = `${global.APIs.adonix.url}/download/instagram?apikey=${global.APIs.adonix.key}&url=${url}`
+        const res = await fetch(api)
+        const json = await res.json()
+        if (json.status && json.data?.length) {
+          data = json.data.map(v => v.url)
+}
+} catch (e) {}
+}
+
+    if (/(facebook\.com|fb\.watch)/i.test(args[0]) &&!data.length) {
+      try {
+        const api = `${global.APIs.adonix.url}/download/facebook?apikey=${global.APIs.adonix.key}&url=${url}`
+        const res = await fetch(api)
+        const json = await res.json()
+        if (json.status && json.result?.media?.video_hd) {
+          data = [json.result.media.video_hd]
+}
+} catch (e) {}
+}
+
+    if (!data.length) {
+      try {
+        const api = `${global.APIs.vreden.url}/api/igdownload?url=${url}`
+        const res = await fetch(api)
+        const json = await res.json()
+        if (json.resultado?.respuesta?.datos?.length) {
+          data = json.resultado.respuesta.datos.map(v => v.url)
+}
+} catch (e) {}
+}
+
+    if (!data.length) {
+      try {
+        const api = `${global.APIs.delirius.url}/download/instagram?url=${url}`
+        const res = await fetch(api)
+        const json = await res.json()
+        if (json.status && json.data?.length) {
+          data = json.data.map(v => v.url)
+}
+} catch (e) {}
+}
+
+    if (!data.length) {
+      return conn.reply(m.chat, `🌾 No se pudo obtener el contenido.`, m)
+}
+
+    for (let media of data) {
+      await conn.sendFile(m.chat, media, 'media.mp4', `🌵 Aquí tienes 🌱`, m)
+      await m.react('🌱')
+}
+
+} catch (error) {
+    await m.react('🌾')
+    await m.reply(`🌵 Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`)
+}
+}
+
+handler.command = ['instagram', 'ig', 'facebook', 'fb']
+handler.tags = ['descargas']
+handler.help = ['instagram', 'ig', 'facebook', 'fb']
+handler.group = true
+
+export default handler
