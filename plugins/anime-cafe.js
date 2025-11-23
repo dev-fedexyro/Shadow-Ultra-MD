a77be55275e28ab1ac806d62aa5332c56ab2098e
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-23 07:36:07 -0300

    Update anime-cafe.js

--- plugins/anime-cafe.js
+++ plugins/anime-cafe.js
@@ -46,7 +46,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 };
 
 handler.help = ['coffee/cafe @tag'];
-handler.tags = ['diversión'];
+handler.tags = ['fun'];
 handler.command = ['coffee', 'cafe', 'taza'];
 handler.group = true;
 
