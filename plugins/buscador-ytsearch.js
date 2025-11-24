469584c9d95ddc7375f845982c4e351e220fabed
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-23 21:46:15 -0300

    Create buscador-ytsearch.js

---
+++ plugins/buscador-ytsearch.js
@@ -0,0 +1,40 @@
+import yts from 'yt-search';
+
+const handler = async (m, { text, conn, command}) => {
+  if (!text) {
+    return conn.reply(m.chat, '\`\`\`🌵 ¿Qué deseas buscar en YouTube.\`\`\`', m);
+}
+
+  conn.reply(m.chat, '\`\`\`🔍 Buscando en YouTube, un momento.\`\`\`', m);
+
+  try {
+    const { all} = await yts(text);
+    const videos = all.filter(v => v.type === 'video');
+
+    if (!videos.length) {
+      return conn.reply(m.chat, '\`\`\`❌ No se encontraron resultados.\`\`\`', m);
+}
+
+    const formattedResults = videos.map(v => (
+      `「✦」*${v.title}*\n` +
+      `🍬 Canal: *${v.author.name}*\n` +
+      `🕝 Duración: *${v.timestamp}*\n` +
+      `📆 Subido: *${v.ago}*\n` +
+      `👀 Vistas: *${v.views.toLocaleString()}*\n` +
+      `🔗 Enlace: ${v.url}`
+)).join('\n\n••••••••••••••••••••••••••••••••••••\n\n');
+
+    await conn.sendFile(m.chat, videos[0].thumbnail, 'yts.jpeg', formattedResults, m);
+} catch (error) {
+    console.error(error);
+    conn.reply(m.chat, '\`\`\`⚠️ Ocurrió un error al buscar en YouTube.\`\`\`', m);
+}
+};
+
+handler.help = ['ytsearch'];
+handler.tags = ['buscador'];
+handler.command = ['ytbuscar', 'ytsearch', 'yts'];
+handler.register = true;
+handler.coin = 1;
+
+export default handler;
