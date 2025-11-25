00b4ab1d122ad232fb29fc664aea4ebfb93bd1cf
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-25 10:58:29 -0300

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,38 +1,20 @@
-const handler = async (m, { isOwner, isAdmin, conn, participants, args, usedPrefix}) => {
-  if (usedPrefix.toLowerCase() === 'a') return;
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
     return;
-}
-
-  const mensaje = args.join(' ').trim();
-  const invocador = m.pushName || 'una sombra anónima';
-  const imagen = 'https://files.catbox.moe/32d81v.jpg';
-
-  const sombras = participants.map(p => `• @${p.id.split('@')[0]}`).join('\n');
-
-  const texto = `
-\`\`\`🌑 Invocación grupal de Shadow\`\`\`
-
-Hola, soy ${invocador}, y he reunido a las sombras...
-
-${mensaje || '_El silencio también es parte del ritual..._'} 🕯️
-
-👥 *Sombras presentes:* ${participants.length}
-${sombras}
-
-Enviando desde el rincón más oscuro del jardín...
-`;
-
-  await conn.sendFile(m.chat, imagen, 'shadow.jpg', texto.trim(), m, false, {
-    mentions: participants.map(p => p.id)
-});
+  }
+  const pesan = args.join` `;
+  const colombia = `\`\`\`💌 Mensaje:\`\`\` ${pesan}`;
+  let teks = `\`\`\`🌵 INVOCANDO GRUPO\`\`\`\n${colombia}\n\n☁️ *Tags:*\n`;
+  for (const mem of participants) {
+    teks += `@${mem.id.split('@')[0]}\n`;
+  }
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
-
-handler.help = ['tagall *<mensaje>*', 'invocar *<mensaje>*'];
-handler.tags = ['group'];
+handler.help = ['tagall *<mesaje>*', 'invocar *<mesaje>*'];
+handler.tags = ['grupo'];
 handler.command = ['tagall', 'invocar'];
 handler.admin = true;
 handler.group = true;
-
 export default handler;
