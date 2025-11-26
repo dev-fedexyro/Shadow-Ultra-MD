39734fcbeacfb41210e4f3b5816ddf75df7ad33c
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-25 22:36:39 -0300

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,10 +1,21 @@
-let handler = async (m, { conn, isAdmin }) => {
-if (m.fromMe) throw 'Nggk'
-if (isAdmin) throw '🌵 _Mi creador ahora es admin_'
-await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")}
-handler.help = ['admin','autoadmin']
-handler.tags = ['owner']
-handler.command = ['admin','autoadmin']
-handler.rowner = true
-handler.botAdmin = true
-export default handler
+let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+    if (isAdmin) {
+        throw '👑 *Ya eres administrador* en este grupo.';
+    }
+
+    if (!isBotAdmin) {
+        throw '🤖 *Necesito ser administrador* para poder otorgarte el permiso.';
+    }
+
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote");
+    
+    m.reply('✅ *¡Hecho!* Ahora eres administrador del grupo.');
+}
+
+handler.help = ['autoadmin'];
+handler.tags = ['owner'];
+handler.command = ['autoadmin'];
+handler.rowner = true;.
+handler.botAdmin = true;
+
+export default handler;
