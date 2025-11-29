b24dbee1e900aa33de388afc28422d2a1d6c901c
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-29 12:44:03 -0300

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -31,14 +31,6 @@ var handler = async (m, { conn }) => {
     let timestamp = speed()
     let latensi = speed() - timestamp
 
-    let _muptime = process.uptime() * 1000
-    let muptime = clockString(_muptime)
-
-    let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-    let groups = Object.entries(conn.chats)
-        .filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce)
-        .map(v => v[0])
-
     let texto = `\`ⓘ ${latensi.toFixed(4)} ms\``
 
     const thumbnailBuffer = await getImageUrlBuffer()
@@ -67,10 +59,3 @@ handler.command = ['ping', 'p']
 handler.register = true
 
 export default handler
-
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
-}
