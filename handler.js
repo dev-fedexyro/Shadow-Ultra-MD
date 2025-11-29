6256f48fa2307201ea812d489b16d8c0221630b5
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-28 22:18:50 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -145,21 +145,6 @@ if (isBannedGlobal && !isROwner) {
     return; 
 }
 
-const primaryChatId = global.db.data.settings.primaryChat;
-const currentChatId = m.chat;
-
-if (primaryChatId && primaryChatId !== currentChatId && !isROwner) { 
-    
-    if (m.isGroup && m.isCommand) {
-        const primaryChatName = await this.getName(primaryChatId) || 'el grupo exclusivo';
-        const exclusiveMsg = `🔒 *Modo Exclusivo Activo*\n\n*${botName}* solo responde comandos en su grupo exclusivo: *${primaryChatName}*.`;
-        conn.reply(m.chat, exclusiveMsg, m);
-    }
-    
-    return; 
-}
-
-
 if (opts["queque"] && m.text && !(isPrems)) {
 const queque = this.msgqueque, time = 1000 * 5
 const previousID = queque[queque.length - 1]
